aaabef51ced036e13aa419169e657cd8e671d1c4
Glitch (hello-express) <none>
2020-06-01 13:28:51 +0000

    🌈🎃 Checkpoint ./public/qwrfr6uit4r.html:2751043/246 ./public/index.html:2751043/467 ./server.js:2751043/635

--- server.js
+++ server.js
@@ -10,7 +10,28 @@ const _message = (code,message,data) =>{
     return {code,message,data};
 }
 
-app.post('/check',(req,res)=>{
+// Q 1
+app.post('/reverse',(req,res)=>{
+
+    const originCode = req.body.code;
+
+    if(originCode.includes('ilovecoding')){
+        res.status(401).send(_message(401,`Opps! you can't cheat this`,{ans:data,next:''}));
+    }
+
+    const plusFunc   = originCode + `\n reverseString('gnidocevoli');`;
+
+    const data = vm.run(plusFunc);
+    
+    if(data !== 'ilovecoding'){
+        res.status(401).send(_message(401,`Opps! it's incorrect.`,{ans:data,next:''}));
+    }
+
+    res.status(200).send(_message(200,`Harray! it's correct!.`,{ans:data,next:'qwrfr6uit4r.html'}));
+})
+
+// Q 2
+app.post('/wrap',(req,res)=>{
 
     const originCode = req.body.code;
 
